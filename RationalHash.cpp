8ca3d093cefa5840d913b2b8b9816ca097f4e3d8
Artem Poliakov <artem4250@gmail.com>
2022-02-14 12:35:27 +0200

    update

--- RationalHash.cpp
+++ RationalHash.cpp
@@ -33,6 +33,12 @@ size_t RationalHash::nearestPrime(const size_t& n)
     }
     return res;
 }
+bool RationalHash::allUniqueNumbers(const std::vector<size_t>& v)
+{
+    std::vector<size_t> tmp = v;
+    sort(tmp.begin(), tmp.end());
+    return std::adjacent_find(tmp.begin(), tmp.end()) == tmp.end();
+}
 
 size_t RationalHash::vectorCompression(const std::vector<Rational>& v)
 {
@@ -54,18 +60,14 @@ std::vector<size_t> RationalHash::inputVectorsCompression(const std::vector<std:
     randomVec.resize(max);
     std::generate(randomVec.begin(), randomVec.end(), RationalHash::randomVecNGen);
 
-    std::vector<size_t> res(input.size()), curr;
-    bool allUnique;
+    std::vector<size_t> res(input.size());
     while(true)
     {
         for(size_t i=0; i<input.size(); i++)
         {
             res[i] = vectorCompression(input[i]);
         }
-        curr = res;
-        sort(curr.begin(), curr.end());
-        allUnique = std::adjacent_find(curr.begin(), curr.end()) == curr.end();
-        if(allUnique) break;
+        if(allUniqueNumbers(res)) break;
         randomVec[0] = randomVecNGen();
     }
     return res;
@@ -79,9 +81,9 @@ void RationalHash::hashing(const std::vector<std::vector<Rational>>& inputV, con
 
     std::vector<std::vector<size_t>> preHash(M);
     size_t currIndex;
-    for(auto i : numbersV)
+    for(size_t i=0; i<numbersV.size(); i++)
     {
-        currIndex = ((A*i+B)%P)%M;
+        currIndex = ((A*numbersV[i]+B)%P)%M;
         preHash[currIndex].push_back(i);
     }
 
@@ -100,11 +102,35 @@ void RationalHash::hashing(const std::vector<std::vector<Rational>>& inputV, con
             secondaryTables[i].m = 1;
             secondaryTables[i].a = 0;
             secondaryTables[i].b = 0;
-            secondaryTables[i].cells = {Cell{inputV[i]}};
+            secondaryTables[i].cells = {Cell{inputV[preHash[i][0]]}};
         }
         else
         {
-
+            secondaryTables[i].m = preHash[i].size()*preHash[i].size(); //square
+            secondaryTables[i].cells.resize(secondaryTables[i].m);
+            bool flag = false;
+            while(true)
+            {
+                secondaryTables[i].a = randN(1, P-1);
+                secondaryTables[i].b = randN(0, P-1);
+                for(size_t k=0; k<preHash[i].size(); k++)
+                {
+                    currIndex = ((secondaryTables[i].a*numbersV[preHash[i][k]]+secondaryTables[i].b)%P)%secondaryTables[i].m;
+                    if(!secondaryTables[i].cells[currIndex].data.empty())
+                    {
+                        flag = true;
+                        break;
+                    }
+                    secondaryTables[i].cells[currIndex] = {inputV[preHash[i][k]]};
+                }
+                if(flag)
+                {
+                    flag = false;
+                    secondaryTables[i].cells.clear();
+                    secondaryTables[i].cells.resize(secondaryTables[i].m);
+                }
+                else break;
+            }
         }
     }
 }
@@ -112,7 +138,7 @@ RationalHash::RationalHash(const std::vector<std::vector<Rational>>& input)
 {
     srand(time(nullptr));
     auto numbers = inputVectorsCompression(input); //randomVec
-    hashing(input, numbers); //M,P,A,B
+    hashing(input, numbers); //M,P,A,B, secondaryTables
 }
 bool RationalHash::contains(const std::vector<Rational>& n)
 {
